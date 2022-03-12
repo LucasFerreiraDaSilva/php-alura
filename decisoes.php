733ae583ff602e46a4abb4ddf5d359e778c5ea3d
Lucas Silva <maniarenu@hotmail.com>
2022-03-12 13:53:34 -0300

    PHP

--- decisoes.php
+++ decisoes.php
@@ -1,12 +1,18 @@
 <?php
-$idade = 21;
-$nome = "Lucas";
+$idade = 11;
+$numeroDePessoas = 1;
 
-echo "Voce so pode entra se tiver parti de 18 anos . <br>"; 
+echo "Voce so pode entra se tiver parti de 18 anos ou <br>";
+echo "a parti de 16 anos acopanhado <br>";
 
-if ($idade == 18 || $idade > 18) {
-echo "voce tem $idade anos e pode entrar no local";
+if ($idade == 18) {
+echo "voce tem $idade anos e pode entrar no local sozinho.";
 
-} else {
-echo "você tem $idade é permitido a entrada de menor no local";
+} else if ($idade >=16 && $numeroDePessoas > 1) {
+    echo "você tem $idade anos, está acompanhado(a), então pode entrar. <br>";
+}else {
+    echo "você tem $idade e não é permitido a entrada de menor no local , apenas com responsavel !";
+
 }
+
+echo "<br><br> Adeus !";
